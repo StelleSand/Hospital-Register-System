f351da774fa9a98e2cc42086f954e86e7c59555c
= <841098845@qq.com>
2015-11-25 16:42:28 +0800

    模型初始化

---
+++ app/Hospital.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v5
+ * Date: 2015/11/25
+ * Time: 13:43
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hospital extends Model{
+
+    protected $table = 'hospital';
+
+    protected $fillable = ['name','description','location','phone'];
+
+}
