71e7c556e2e8aa4517fc9e067d77e21aa95d4244
LesWang <wangle1321@163.com>
2015-12-23 01:49:01 +0800

    change triage check form

--- resources/views/hospital_admin/hospital_triage.blade.php
+++ resources/views/hospital_admin/hospital_triage.blade.php
@@ -1,6 +1,11 @@
 @extends("master")
 @section("title","历史订单")
+@section("style")
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap-datetimepicker.css">
+@stop
 @section("script")
+    <script type="text/javascript" src="./js/bootstrap-datetimepicker.js"></script>
+    <script type="text/javascript" src="./js/bootstrap-datetimepicker.zh-CN.js"></script>
     <script>
         $(function(){
             $('.form_date').datetimepicker({
@@ -13,38 +18,67 @@
             minView: 2,
             forceParse: 0
         })
+        })
+        function change_input(value){
+            $("#check_box").empty();
+            if(value=="no")
+                return;
+            else{
+                var label=$("<label></lable>");
+                var input=$("<input>",{
+                    type:"text"
+                }).addClass("form-control");
+                if(value=="office"){
+                    label.text("请输入科室")
+                    input.attr("name","office");
+                }
+                if(value=="doctor"){
+                    label.text("请输入医生")
+                    input.attr("name","doctor");
+                }
+                if(value=="id"){
+                    label.text("请输入订单号")
+                    input.attr("name","order_id");
+                }
+                if(value=="name"){
+                    label.text("请输入用户姓名");
+                    input.attr("name","user_name");
+                }
+                $("#check_box").append(label).append(input);
+            }
+        }
     </script>
 @stop
 @section("content")
     <div class="page-header"><h1>分诊台管理</h1></div>
         <form class="form-inline">
-            <div class="form-group">
-                <label for="input2" class="control-label">选择日期</label>
-                <div class="input-group date form_date" data-date data-date-format="yyyy年mm月dd日" data-link-field="dtp_input2" data-link-format="yyyy-m-d">
-                    <input class="form-control" id="input1"  type="text" name="date_to_show" readonly>
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
+            <div class="row">
+                <div class="col-md-4">
+                <div class="form-group">
+                    <label for="input2" class="control-label">请选择日期</label>
+                    <div class="input-group date form_date" data-date data-date-format="yyyy年mm月dd日" data-link-field="input2" data-link-format="yyyy-m-d">
+                        <input class="form-control" id="input1"  type="text" name="date_to_show" readonly>
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
+                        <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
+                    </div>
+                    <input type="hidden" id="input2" name="date">
                 </div>
-                <input type="hidden" id="input2" name="date">
+                </div>
+                <div class="col-md-4">
+                <label>请选择筛选类型</label>
+                <select name=sel onchange="change_input(this.options[this.options.selectedIndex].value)" class="form-control">
+                    <option value="no">请选择</option>
+                    <option value="office">选择科室</option>
+                    <option value="doctor">选择医生</option>
+                    <option value="id">订单号</option>
+                    <option value="name">预约人姓名</option>
+                </select>
+                </div>
+                <div class="col-md-4">
+                <div class="form-group" id="check_box"></div>
+                </div>
             </div>
-            <!--这里也可以使用select-->
-            <div class="form-group">
-                <label>选择科室</label>
-                <input type="text" class="form-control check_list" name="office">
-            </div>
-            <div class="form-group">
-                <label>选择医生</label>
-                <input type="text" class="form-control check_list" name="doctor">
-            </div>
-            <div class="form-group">
-                <label>订单号</label>
-                <input type="text" class="form-control check_list" name="order_id">
-            </div>
-            <div class="form-group">
-                <label>预约人姓名</label>
-                <input type="text" class="form-control check_list" name="user_name">
-            </div>
-            <button class="btn btn-primary" type="submit">查询</button>
+            <div class="row text-right"><button class="btn btn-primary" type="submit">查询</button></div>
         </form>
         <br/>
         <table class="table table-striped text-center">
@@ -63,9 +97,25 @@
                     <td>{{$order->id}}</td>
                     <td>{{$order->user->name}}</td>
                     <td>{{$order->doctor->office->name}}</td>
-                    <td>{{$order->doctor->name}}</td>
-                    <td>{{$order->appoint_time}}</td>
-                    <td>{{$order->status}}</td>
+                    <td>{{$order->doctor->user->name}}</td>
+                    <td>{{$order->appoint_date}}</td>
+                    <td>
+                        @if ($order->state == 'ordered')
+                            已预订未付款
+                        @elseif($order->state == 'order_canceled')
+                            订单已取消
+                        @elseif($order->state == 'payed')
+                            已付款可挂号
+                        @elseif($order->state == 'payment_canceled')
+                            已退款
+                        @elseif($order->state == 'triage_checked')
+                            分诊台已核实
+                        @elseif($order->state == 'doctor_checked')
+                            就诊医生已核实
+                        @elseif($order->state == 'completed')
+                            订单完成
+                        @endif
+                    </td>
                     <td><button class="btn btn-primary">打印预约单</button></td>
                 </tr>
             @endforeach
