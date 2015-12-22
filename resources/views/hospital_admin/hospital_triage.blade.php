ce888e81407d591425421a4090d52feceeac360b
LesWang <wangle1321@163.com>
2015-12-23 02:11:48 +0800

    a little change

--- resources/views/hospital_admin/hospital_triage.blade.php
+++ resources/views/hospital_admin/hospital_triage.blade.php
@@ -53,7 +53,7 @@
     <div class="page-header"><h1>分诊台管理</h1></div>
         <form class="form-inline">
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-6">
                 <div class="form-group">
                     <label for="input2" class="control-label">请选择日期</label>
                     <div class="input-group date form_date" data-date data-date-format="yyyy年mm月dd日" data-link-field="input2" data-link-format="yyyy-m-d">
@@ -64,9 +64,23 @@
                     <input type="hidden" id="input2" name="date">
                 </div>
                 </div>
-                <div class="col-md-4">
-                <label>请选择筛选类型</label>
-                <select name=sel onchange="change_input(this.options[this.options.selectedIndex].value)" class="form-control">
+                <div class="col-md-6">
+                    <label>请选择订单状态</label>
+                    <select name="state" class="form-control">
+                        <option value="payed">已付款可挂号</option>
+                        <option value="">无限制</option>
+                        <option value="triage_checked">分诊台已核实</option>
+                        <option value="doctor_checked">就诊医生已核实</option>
+                        <option value="completed">订单完成</option>
+                        <option value="ordered">已预订未付款</option>
+                        <option value="order_canceled">订单已取消</option>
+                        <option value="payment_canceled">已退款</option>
+                    </select>
+                </div>
+                <div class="col-md-6">
+                <br/>
+                <label for="select1">请选择筛选类型</label>
+                <select name=sel onchange="change_input(this.options[this.options.selectedIndex].value)" class="form-control" id="select1">
                     <option value="no">请选择</option>
                     <option value="office">选择科室</option>
                     <option value="doctor">选择医生</option>
@@ -74,8 +88,9 @@
                     <option value="name">预约人姓名</option>
                 </select>
                 </div>
-                <div class="col-md-4">
-                <div class="form-group" id="check_box"></div>
+                <div class="col-md-6">
+                    <br/>
+                    <div class="form-group" id="check_box"></div>
                 </div>
             </div>
             <div class="row text-right"><button class="btn btn-primary" type="submit">查询</button></div>
@@ -92,32 +107,32 @@
                 <td>操作</td>
             </th>
             @foreach($orders as $order)
-                <tr>
-                    <td></td>
-                    <td>{{$order->id}}</td>
-                    <td>{{$order->user->name}}</td>
-                    <td>{{$order->doctor->office->name}}</td>
-                    <td>{{$order->doctor->user->name}}</td>
-                    <td>{{$order->appoint_date}}</td>
-                    <td>
-                        @if ($order->state == 'ordered')
-                            已预订未付款
-                        @elseif($order->state == 'order_canceled')
-                            订单已取消
-                        @elseif($order->state == 'payed')
-                            已付款可挂号
-                        @elseif($order->state == 'payment_canceled')
-                            已退款
-                        @elseif($order->state == 'triage_checked')
-                            分诊台已核实
-                        @elseif($order->state == 'doctor_checked')
-                            就诊医生已核实
-                        @elseif($order->state == 'completed')
-                            订单完成
-                        @endif
-                    </td>
-                    <td><button class="btn btn-primary">打印预约单</button></td>
-                </tr>
-            @endforeach
+            <tr>
+                <td></td>
+                <td>{{$order->id}}</td>
+                <td>{{$order->user->name}}</td>
+                <td>{{$order->doctor->office->name}}</td>
+                <td>{{$order->doctor->user->name}}</td>
+                <td>{{$order->appoint_date}}</td>
+                <td>
+                    @if ($order->state == 'ordered')
+                        已预订未付款
+                    @elseif($order->state == 'order_canceled')
+                        订单已取消
+                    @elseif($order->state == 'payed')
+                        已付款可挂号
+                    @elseif($order->state == 'payment_canceled')
+                        已退款
+                    @elseif($order->state == 'triage_checked')
+                        分诊台已核实
+                    @elseif($order->state == 'doctor_checked')
+                        就诊医生已核实
+                    @elseif($order->state == 'completed')
+                        订单完成
+                    @endif
+                </td>
+                <td><button class="btn btn-primary">打印预约单</button></td>
+            </tr>
+        @endforeach
         </table>
 @stop
