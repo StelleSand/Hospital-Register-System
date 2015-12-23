f9854c934162b7e7f9017699a3f7ca2c4f3048ec
= <841098845@qq.com>
2015-12-23 13:22:08 +0800

    regist and login bug fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,9 +23,11 @@ Route::post('submitOrder','HospitalController@ajaxSubmitOrder');
 Route::get('login', 'Auth\AuthController@getLogin');
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('login', 'Auth\AuthController@postLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('register', 'Auth\AuthController@getRegister');
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('register', 'Auth\AuthController@postRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 Route::get('logout', 'Auth\AuthController@getLogout');
 
 //管理员相关路由
